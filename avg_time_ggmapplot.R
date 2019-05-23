c5908d5c7cb1f0b6806d3bf5add86b1c68926a5f
GISH <willyshao514@gmail.com>
2019-05-23 12:00:10 +0800

    upload what ive done

---
+++ avg_time_ggmapplot.R
@@ -0,0 +1,44 @@
+library(rethinking)
+library(dplyr)
+library(Hmisc)
+
+
+library(ggmap)
+library(mapproj)
+library(DataTaipei)
+library(dplyr)
+
+
+
+df <- read.csv("201601.txt",
+               sep="")
+
+register_google(key = "AIzaSyB15Z9DEJCtKe6BzE6WkkW7qaF20jiThgg", write = TRUE)
+
+
+
+
+map <- get_map(location = 'Taipei', zoom = 12, maptype = "roadmap", language = "zh-TW")
+ggmap(map)
+
+
+
+slatlong <- read.csv("station name with lat and long.csv")
+library(stringr)
+slatlong$rent_sta<- str_sub(slatlong$rent_sta,8,-1) # trim out "Taiwan" characters
+
+avgtime_data <- read.csv("C:/Users/willy/Desktop/ntu mba/grade 1 semester two/multivariate analysis/QBS group project/avg_time.csv")
+
+
+# merge avg_time and latlong dataframes
+time_lat_and_long <- merge(slatlong,avgtime_data,by.x="rent_sta")
+  
+ggmap(map)+geom_point(aes(x=lat,y=long,size=Sec),data=time_lat_and_long)
+
+#trim out second > 20000 datas
+
+t_clean <-time_lat_and_long[time_lat_and_long$Sec < 20000,]
+ggmap(map)+geom_point(aes(x=lat,y=long,size=Sec),data=t_clean)
+
+
+write.table(s_lat_and_long,file=".csv",sep=",")
---
+++ lat_and_long.R
@@ -0,0 +1,46 @@
+library(rethinking)
+library(dplyr)
+library(ggmap)
+
+
+
+
+df <- read.csv("C:/Users/willy/Desktop/ntu mba/grade 1 semester two/multivariate analysis/QBS group project/201601.txt",
+               sep="")
+
+
+#TO MAKE SURE THIS GOOGLEMAP TO FIND THE RIGHT PLACE, I ADD TAIWAN FOR EACH STATION NAME
+df2 <- df
+df2$rent_sta <- lapply(df2$rent_sta, function(x) paste("Taiwan", x, sep = " "))
+
+
+
+
+
+#find all station names in data and its lat and long
+station_names <- unique(df2$rent_sta)
+#lat_and_long <- geocode('Taiwan Xingya Jr. High School')
+lat_and_long = data.frame()
+
+
+
+for (i in station_names){
+  lat_and_long <- rbind(lat_and_long,geocode(i)) # 這邊還是有可能googlemap抓錯經緯度，估計是中文轉英文站點問題，我棄療
+}
+
+#s_lat_and_long為一個有rent_sta跟其相關之經緯度的dataframe
+s_lat_and_long = data.frame(matrix(ncol = 3, nrow = 0))
+x <- c("rent_sta","lat","long")
+colnames(s_lat_and_long) <- x
+
+count <- 1
+# record each stattion with its lat and long 
+for (i in station_names){
+  s_lat_and_long[count,1] <- i
+  s_lat_and_long[count,2] <- lat_and_long[count,1] 
+  s_lat_and_long[count,3] <- lat_and_long[count,2]
+  count <- count+1
+}
+
+
+write.table(s_lat_and_long,file="station name with lat and long.csv",sep=",")
